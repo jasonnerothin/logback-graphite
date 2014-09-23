2ebe6d973164970112304156d3e6dc96840b1604
Jason Nerothin <jasonnerothin@gmail.com>
2014-09-23 01:40:58 -0500

    throwing more data at graphite to make it more apparent that data are there behind the graphs

--- src/test/java/com/jasonnerothin/logging/PlainTextGraphiteAppenderI10nTest.java
+++ src/test/java/com/jasonnerothin/logging/PlainTextGraphiteAppenderI10nTest.java
@@ -7,6 +7,8 @@ import org.slf4j.LoggerFactory;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Random;
+import java.util.TimeZone;
 
 /**
  * Created by IntelliJ IDEA.
@@ -17,48 +19,83 @@ import java.util.Date;
  */
 public class PlainTextGraphiteAppenderI10nTest {
 
-    private Logger logger = LoggerFactory.getLogger(getClass());
+    private static final int NUM_ITERATIONS = 120;
 
-    private static final long KEEP_ALIVE_MS = Long.MAX_VALUE;
-//    private static final long KEEP_ALIVE_MS = 2;
+    private Logger logger = LoggerFactory.getLogger(getClass());
+    private Random random = new Random(System.currentTimeMillis());
+    private TestMetric[] kids;
 
     @Before
-    public void setup(){
-        System.setProperty(PlainTextGraphiteAppender.GRAPHITE_HOST_PROPERTY_NAME, "0.0.0.0");
+    public void setup() {
+
+        System.setProperty(PlainTextGraphiteAppender.GRAPHITE_HOST_PROPERTY_NAME, "192.168.33.10");
         System.setProperty(PlainTextGraphiteAppender.GRAPHITE_PORT_PROPERTY_NAME, "2003");
+
+        kids = new TestMetric[3];
+        kids[0] = elida();
+        kids[1] = ruby();
+        kids[2] = peter();
+
     }
 
     @Test
-    public void testWrite() throws InterruptedException {
-
-        TestMetric elida = new TestMetric();
-        elida.name = "nerothin.elida.age";
-        elida.data = 8.999999999;
-
-        TestMetric ruby = new TestMetric();
-        ruby.name = "nerothin.ruby.age";
-        ruby.data = 6l;
-
-        TestMetric peter = new TestMetric();
-        peter.name = "nerothin.peter.age";
-        peter.data = 3.3f;
-
-        logger.info(elida.toString());
-        logger.info(ruby.toString());
-        logger.info(peter.toString());
-
-//        doNotReturn();
+    public void testWriteOnce() throws InterruptedException {
+        logKids();
     }
 
+    private void logKids() {
+        for (TestMetric kid : kids)
+            logger.info(kid.toString());
+    }
 
-
-    private void doNotReturn() {
-        try{
-            Thread.currentThread();
-            Thread.sleep(KEEP_ALIVE_MS);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
+    @Test
+    public void testWriteForAWhile() throws InterruptedException {
+        for (int i = 0; i < NUM_ITERATIONS; i++) {
+            adjustKids();
+            logKids();
+            Thread.sleep(1000);
         }
+    }
+
+    private void adjustKids() {
+        for (TestMetric kid : kids) {
+            adjust(kid);
+        }
+    }
+
+    public void adjust(TestMetric kid) {
+        Number number = kid.data;
+        kid.data = number.floatValue() + randomNum();
+    }
+
+    /**
+     * @return between (-1, 1)
+     */
+    private float randomNum() {
+        float pct = random.nextInt(100) / 100f;
+        if (random.nextBoolean()) pct = pct * -1;
+        return pct;
+    }
+
+    private TestMetric peter() {
+        TestMetric peter = new TestMetric();
+        peter.name = "nerothin.age.peter";
+        peter.data = 3.3d;
+        return peter;
+    }
+
+    private TestMetric ruby() {
+        TestMetric ruby = new TestMetric();
+        ruby.name = "nerothin.age.ruby";
+        ruby.data = 6l;
+        return ruby;
+    }
+
+    private TestMetric elida() {
+        TestMetric elida = new TestMetric();
+        elida.name = "nerothin.age.elida";
+        elida.data = 8.99;
+        return elida;
     }
 
 }
@@ -72,7 +109,11 @@ class TestMetric {
 
     @Override
     public String toString() {
-        return name + DELIMITER + data;
+        return name + DELIMITER + data + DELIMITER + unixTime();
+    }
+
+    private long unixTime() {
+        return System.currentTimeMillis() / 1000;
     }
 
 }
